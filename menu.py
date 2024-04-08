6daa074070970fd5abfbfbb5b27a2fa2e500d54c
Inderpaljaswal <160301233+Inderpaljaswal@users.noreply.github.com>
2024-04-07 20:56:09 -0400

    Update menu.py

--- menu.py
+++ menu.py
@@ -59,4 +59,28 @@ def generate_reports_menu():
 if__name__=="__main__":
    main()
 
+
+def main_menu()
+while True:
+    print("\nEmployee Management System")
+    print("1. Add Employee")
+    print("2. Delete Employee")
+    print("3. Update Employee")
+    print("4. Generate Reports")
+    print("5. Exit")
+    choice = input("Enter your choice")
+
+    if choice.isdigit():#check if input is digit
+        choice= int(choice)
+        if choice == 1:
+            add_employee()
+        elif choice == 2:
+            delete_employee()
+        elif choice == 3:
+            update_employee()
+        elif choice == 4:
+            generate_report()
+        elif choice == 5:
+            print("Exiting the program .Goodbye")
+            
         
