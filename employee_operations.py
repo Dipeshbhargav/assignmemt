3060d7b8980db568f69286b99c190c057fc118bd
kaursimmu56 <162753436+kaursimmu56@users.noreply.github.com>
2024-04-07 21:29:10 -0400

    Update employee_operations.py

--- employee_operations.py
+++ employee_operations.py
@@ -4,7 +4,45 @@ Employee Operations Module
 This module provides functions for adding, deleting, and updating employee information.
 """
 
+def add_employee():"""
+Employee Operations Module
+
+This module provides functions for adding, deleting, and updating employee information.
+"""
+
 def add_employee():
+    #Add Employee function
+    #input details for a new employee and adds the new employee
+    id = int(input("please enter the ID for the new employee:"))
+    firstName = input("please enter the employee's firstname")
+    last_name = input("please enter the employee's lastname")
+    date_of_birth = input("please enter the employee's date of birth in YYYY/MM/DD")
+    start_of_year = int(input("please enter the employee's starting year")
+    position = input("please enter the employee's position")
+    salary = float(input("please enter the employee's salary")
+
+    """
+   
+    
+    This function prompts the user to  the employee to the system.
+    
+    """
+def delete_employee():
+
+    {"id": "001", "first_name": "John", "last_name": "Doe"}
+    {"id": "002", "first_name": "Alice", "last_name": "Smith"}
+    {"id": "003", "first_name": "Bob", "last_name": "Johnson"} 
+    {"id": "004", "first_name": "Mukul", "last_name": "Sharma"}
+    {"id": "005", "first_name": "Sophia", "last_name": "Davis"}
+    {"id": "006", "first_name": "Michael", "last_name": "Brown"}
+    
+    """
+    Delete Employee Function
+
+    This function prompts the user to input the ID of the employee to be deleted and
+    removes the employee from the system.
+
+    """
     id = int(input("please enter the ID for the new employee:")
     firstName = input("please enter the employee's firstname")
     last_name = input("please enter the employee's lastname")
